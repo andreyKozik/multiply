e0605b6063330ea6f7d556bad301e4e3f43696a6
Andrei Kozik <13pgs1.kozik.a@pdu.by>
2019-10-03 13:07:27 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function multiply(first, second) {
-  // your solution
+module.exports = function multiply(a, b) {
+    var result = [];
+    for (var i = a.length - 1; i >= 0; i--) {
+        for (var j = b.length - 1; j >= 0; j--) {
+            var m = a[i] * b[j];
+            result[i + j] = (result[i + j] > 0) ? result[i + j] + m : m;
+        }
+    }
+    for (var i = result.length - 1; i >= 0; i--) {
+        if (i > 0) {
+            result[i - 1] = result[i - 1] + Math.floor(result[i] / 10);
+            result[i] = result[i] % 10;
+        }
+    }
+    return result.join('');
 }
